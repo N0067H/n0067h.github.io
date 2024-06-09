4d2edf9a19300fa555addf6159d8fb387b5445b8
NoobTh <n006h3417h@gmail.com>
2024-06-09 16:39:48 +0900

    Remake

---
+++ scripts/contents.js
@@ -0,0 +1,68 @@
+const yearContents = [
+    // 2018
+    `
+        <p>I learned <b>C++</b> as my first programming language.</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+    `,
+
+    // 2019
+    `
+        <p>I learned <b>C#</b> and the <b>.NET framework</b> to create <b>WinForms</b> applications.</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
+        <br>
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white">
+    `,
+
+    // 2020
+    `
+        <p>I studied the <b>Qt framework</b> to develop GUIs using C++.</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
+        <br>
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white">
+        <img src="https://img.shields.io/badge/Qt-%23217346.svg?style=for-the-badge&logo=Qt&logoColor=white">
+    `,
+
+    // 2021
+    `
+        <p>I learned the <b>Windows API</b> for <b>network socket programming</b>.</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
+        <br>
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white">
+        <img src="https://img.shields.io/badge/Qt-%23217346.svg?style=for-the-badge&logo=Qt&logoColor=white">
+    `,
+
+    // 2022
+    `
+        <p>I learned <b>Programming Language Theory (PLT)</b> to make my own languages.</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
+        <br>
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white">
+        <img src="https://img.shields.io/badge/Qt-%23217346.svg?style=for-the-badge&logo=Qt&logoColor=white">
+    `,
+
+    // 2023
+    `
+        <p>I learned the <b>Rust</b> language to use instead of C++.</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
+        <img src="https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&logo=rust&logoColor=white">
+        <br>
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white">
+        <img src="https://img.shields.io/badge/Qt-%23217346.svg?style=for-the-badge&logo=Qt&logoColor=white">
+    `,
+
+    // 2024
+    `
+        <p>Wait for a year. There's more to come in my journey. :D</p>
+        <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
+        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
+        <img src="https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&logo=rust&logoColor=white">
+        <br>
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white">
+        <img src="https://img.shields.io/badge/Qt-%23217346.svg?style=for-the-badge&logo=Qt&logoColor=white">
+    `,
+]
---
+++ scripts/journey.js
@@ -0,0 +1,11 @@
+
+const changeYear = (element) => {
+    const yearDisplay = document.getElementById("year-display")
+    yearDisplay.textContent = element.value
+
+    const yearContent = document.getElementById("year-content")
+    yearContent.innerHTML = yearContents[element.value - 2018]
+}
+
+changeYear(document.getElementById("year-changer"))
+
