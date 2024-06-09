c2062071df86334fb742554578c25a6a61b1c498
ㅅㅡㅇㅇㅕㅂ <144782226+N0067H@users.noreply.github.com>
2024-06-10 00:50:59 +0900

    Update

--- scripts/contents.js
+++ scripts/contents.js
@@ -16,7 +16,7 @@ const yearContents = [
 
     // 2020
     `
-        <p>I studied the <b>Qt framework</b> to develop GUIs using C++.</p>
+        <p>I learned the <b>Qt framework</b> to develop GUIs using C++.</p>
         <img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white">
         <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white">
         <br>
